869fee4bc69b76a0b7e855e4038c52d0dac492a8
Fedor Zhilkin <fzhilkin@mail.ru>
2020-06-08 18:10:44 +0300

    Add comments

--- 2_debugger.asm
+++ 2_debugger.asm
@@ -15,83 +15,83 @@ old_int1_offset	dw	0
 old_int1_cs	dw	0
 
 int1_handler:
-	push	bp 	;save bp
-	mov	bp, sp  ;copy stack-pointer(sp) into bp 
+	push	bp 	; вносим bp (base pointer) в стек 
+	mov	bp, sp  ; копируем содержимое stack-pointer(sp) в bp 
 
-	pusha		;save AX, CX, ... , in stack
-	push	ds	;ds -- Data segment
+	pusha		; помещаем в стек значения всех 16-битных регистров общего назначения
+	push	ds	; вносим ds (Data segment) в стек
 
-	push	cs	;cs -- Code segment
-	pop	es	;es -- Extra segment
+	push	cs	; вносим cs (Code segment) в стек
+	pop	es	    ; извлекаем операнд из стека в es -- Extra segment
 	push	cs
-	pop	ds	
+	pop	ds	    
 
-	lea	di, outmes	;di = outmes
-	mov	bx, [bp+4]	;cs to print
+	lea	di, outmes		; di = outmes
+	mov	bx, [bp+4]		; копируем содержимое [bp+4] в bx (base register), cs to print
 	call	h4
 	lea	di, [outmes+5]	 
-	mov	bx, [bp+2]	;ip to print
-	call	h4		
+	mov	bx, [bp+2]		; ip to print
+	call	h4 			; вызов процедуры h4	
 
-	lea	dx, outmes
-	mov	ah, 09h
-	int	21h
+	lea	dx, outmes  	; dx (data register)
+	mov	ah, 09h     	; ah -- AX (primary accumulator) high, 09h -- вывод строки
+	int	21h         	; прерывание
 
 	pop	ds
-	popa
+	popa		   		; извлекаем из стека значения всех 16-битных регистров общего назначения
 
 	pop	bp
-	iret
+	iret		   		; возврат из прерывания при 16-битном размере операнда
 
 start:
-	mov	ax, 3D00h	; Open file
+	mov	ax, 3D00h	; открываем файл
 	lea	dx, filename
 	int	21h	
 	
-	mov	bx, ax		; Read file
+	mov	bx, ax		; читаем файл
 	mov	ah, 3Fh
 	mov	cx, 100h
 	lea	dx, prog
 	int	21h
 
-	mov	ah, 3Eh		; Close file
+	mov	ah, 3Eh		; закрываем файл
 	int	21h
 
-	mov	ah, 35h		;save old 1 interrupt
+	mov	ah, 35h		; сохраянем прошлое прерывание 
 	mov	al, 1
 	int	21h
 	mov	old_int1_offset, bx
 	mov	old_int1_cs, es
 
-	mov	ax, 2501h	;writer new 1 interrupt
+	mov	ax, 2501h	; записываем новое прерывание 
 	lea	dx, int1_handler	
 	int	21h
 
-	pushf			; Change flags register
+	pushf			; вносим в стек значения регистров флагов
 	pop	dx
-	or	dx, 100h	; TF:=1	
+	or	dx, 100h	; TF := 1	
 
 	mov	bx, 100h
 
 	lea	cx, psp
-	shr	cx, 4
+	shr	cx, 4       ; свдиг cx вправо 4 раза
 
 	mov	ax, cs
-	add	ax, cx
+	add	ax, cx      ; ax := ax + cx
 	
 	push	ax
 	pop	ds
 
 	pushf
-	push	cs			; address for called program
+	push	cs				; адресс вызываемой программы
 	lea	cx, after_debug		; to return to
 	push	cx
-	xor	cx, cx ;0
+	xor	cx, cx 				; 0
 	push	cx
 
-	push	dx	;called flags
-	push	ax	;code segment
-	push	bx	;100 - ip 
+	push	dx				; called flags
+	push	ax				; code segment
+	push	bx				; 100 - ip 
 	iret
 
 after_debug:
@@ -116,12 +116,12 @@ h2:	mov	al, bl
 	shr	al, 4
 	call	h1
 	mov	al, bl
-h1:			; al - number, di - destination
-	and	ax, 0fh
-	cmp	al, 10
-	sbb	al, 69h
-	das
-	stosb
+h1:					; al - number, di - destination
+	and	ax, 0fh		
+	cmp	al, 10 		; al ?= 10
+	sbb	al, 69h		; вычитание с заемом
+	das				; десятичная коррекция в регистре AL после вычитания
+	stosb			; запись байта в строку
 	ret
 
 align	16
